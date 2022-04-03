884fe26eb6bdc19c9fcf7b3156b565a4c968d75d
tonyketcham <tony@karmalies.studio>
2022-04-03 03:35:53 -0500

    wip: doesn't work yet, may experiment with adding directly in p5-svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import P5 from 'p5-svelte';
+	import p5SvgRuntime from 'p5.js-svg';
+	import type { default as p5Type } from 'p5';
+
+	interface P5Svg extends p5Type {
+		/**
+		 * SVG runtime for the p5.js canvas.
+		 */
+		SVG: p5Type.RENDERER;
+	}
+
+	type SvgSketch = (p5Svg: P5Svg) => void;
+
+	const sketch: SvgSketch = (p5) => {
+		// p5SvgRuntime(p5);
+		p5.setup = () => {
+			// US Letter size paper (8.5 x 11 in) @ 96 DPI
+			p5.createCanvas(816, 1054, p5.SVG);
+		};
+		console.log(p5.SVG);
+		p5.draw = () => {
+			p5.clear(0, 0, 0, 0);
+			p5.ellipse(p5.mouseX, p5.mouseY, 80, 80);
+		};
+	};
+</script>
+
+<P5 {sketch} />
